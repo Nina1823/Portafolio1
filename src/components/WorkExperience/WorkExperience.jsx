a4ae68fa136976f0772f52f144605640410aab91
Nina1823 <lilianarivera310@gmail.com>
2024-07-06 00:28:21 -0500

    Feat: work experience

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -5,19 +5,17 @@ export const WorkExperience =()=>{
     return(
         <div className={styles.container}>
             <h1 className={styles.title}>Experiencia laboral</h1>
-            <div>
-                <div className={styles.backContainer}>
-                    <div className={styles.card}>
-                        <h3>Auditoria y prueba</h3>
-                        <h4>NetW Consulting SAS</h4>
-                    </div>
-                </div>    
-                    <br/>
-                <div className={styles.backContainer}>
-                    <div className={styles.card}>
-                        <h3 className={styles.titleCard}>Diseñadora y desarrolladora web</h3>
-                        <h4>Cardados y Fibras SAS</h4>
-                    </div>
+            <div className={styles.cards}>
+                <div className={styles.cardOne}>
+                    <h3>Auditoria y prueba</h3>
+                    <h4>NetW Consulting SAS</h4>
+                    <h4>2023-2024</h4>
+                </div>  
+                <br/>
+                <div className={styles.cardTwo}>
+                    <h3 className={styles.titleCard}>Diseñadora y desarrolladora web</h3>
+                    <h4>Cardados y Fibras SAS</h4>
+                    <h4>2024</h4>
                 </div>
             </div>
         </div>
